6c5e1198cb4b3859dfc1d8d88eab3bd68c457d58
Русанова Анна <rusanova.annette@gmail.com>
2022-04-14 09:23:49 +0100

    together2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,9 +10,9 @@ router_version_1 = routers.DefaultRouter()
 router_version_1.register(r'categories', CategoryViewSet)
 router_version_1.register(r'genres', GenreViewSet)
 router_version_1.register(r'titles', TitleViewSet)
-router_version_1.register(r'titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet, basename='reviews')
-router_version_1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)' #review_ttl
+router_version_1.register(r'titles/(?P<title_id>\d+)/review_ttl',
+                ReviewViewSet, basename='review')
+router_version_1.register(r'titles/(?P<title_id>\d+)/review_ttl/(?P<review_id>\d+)' #review_ttl
                 r'/comments', CommentViewSet, basename='comments')
 router_version_1.register(r'users', UserViewSet)
 
