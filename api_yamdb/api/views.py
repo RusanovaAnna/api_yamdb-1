b689c0407ff1417ec03b3c788dc98fa2b84b2ba2
Максим Николаев <kononart9@gmail.com>
2022-04-16 21:54:09 +0300

    jjj

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.tokens import default_token_generator
+# from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
@@ -7,7 +7,7 @@ from rest_framework import status, viewsets, mixins, filters, views
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-# from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Category, Genre, Review, Title, User
 from .permissions import (IsAdminOrReadOnly,
@@ -46,7 +46,7 @@ def get_token(request):
         if serializer.is_valid():
             user = get_object_or_404(
                 User, username=serializer.data['username'])
-            token = default_token_generator.make_token(user)
+            token = AccessToken.for_user(user)
             return Response({'token': str(token)}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
