1451c94cdf8caa8dc723ac560787fc3e6cb1ef71
Максим Николаев <kononart9@gmail.com>
2022-04-16 21:32:27 +0300

    kkk

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,7 @@ from rest_framework import status, viewsets, mixins, filters, views
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
+# from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Category, Genre, Review, Title, User
 from .permissions import (IsAdminOrReadOnly,
