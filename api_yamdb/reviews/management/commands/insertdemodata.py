1e08b46e63fa770fba00cbf3846ba1df8bf02f07
Максим Николаев <kononart9@gmail.com>
2022-04-21 04:53:16 +0300

    fix_manag_com

--- api_yamdb/reviews/management/commands/insertdemodata.py
+++ api_yamdb/reviews/management/commands/insertdemodata.py
@@ -3,24 +3,27 @@ import csv
 from django.core.management.base import BaseCommand
 from sqlalchemy import MetaData, Table, create_engine
 
+from api_yamdb.settings import DATABASES
+
 
 class Command(BaseCommand):
     help = "Загружает в базу данных тестовые данные"
 
+    def add_arguments(self, parser):
+        parser.add_argument('engine_db', type=str, help=u'вид базы данных')
+
     def handle(self, *args, **kwargs):
-        insert_data()
+        engine_db = kwargs['engine_db']
+        db1 = DATABASES['default']['NAME']
+        engine = create_engine(str(engine_db) + ':////' + str(db1))
+        print(str(engine))
+        metadata = MetaData()
+        reviews_genre = Table(
+            'reviews_genre', metadata, autoload=True, autoload_with=engine)
+        insert_query = reviews_genre.insert()
 
-
-def insert_data():
-    engine = create_engine('sqlite:///db.sqlite3')
-    metadata = MetaData()
-    reviews_genre = Table(
-        'reviews_genre', metadata, autoload=True, autoload_with=engine)
-    insert_query = reviews_genre.insert()
-
-    with open('static/data/genre.csv', 'r', encoding="utf-8") as file:
-        csv_reader = csv.reader(file, delimiter=",")
-        engine.execute(
-            insert_query,
-            ([{'id': row[0], 'name': row[1], 'slug': row[2]}
-             for row in csv_reader]))
+        with open('static/data/genre.csv', 'r', encoding="utf-8") as file:
+            csv_reader = csv.reader(file, delimiter=",")
+            engine.execute(insert_query,
+                           ([{'id': row[0], 'name': row[1], 'slug': row[2]}
+                            for row in csv_reader]))
