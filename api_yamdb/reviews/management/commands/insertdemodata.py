c77b3f7ecd08e785f42da4edcc1ac4ec740f3b35
Максим Николаев <kononart9@gmail.com>
2022-04-21 12:41:26 +0300

    fix_command_1

--- api_yamdb/reviews/management/commands/insertdemodata.py
+++ api_yamdb/reviews/management/commands/insertdemodata.py
@@ -1,28 +1,31 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from sqlalchemy import MetaData, Table, create_engine
-
-from api_yamdb.settings import DATABASES
+from reviews.models import Category, Genre
 
 
 class Command(BaseCommand):
     help = "Загружает в базу данных тестовые данные"
 
     def add_arguments(self, parser):
-        parser.add_argument('engine_db', type=str, help=u'вид базы данных')
+        parser.add_argument(
+            'table', type=str, nargs='*', help=u'таблица', default='')
 
     def handle(self, *args, **kwargs):
-        engine_db = kwargs['engine_db']
-        db1 = DATABASES['default']['NAME']
-        engine = create_engine(str(engine_db) + ':////' + str(db1))
-        metadata = MetaData()
-        reviews_genre = Table(
-            'reviews_genre', metadata, autoload=True, autoload_with=engine)
-        insert_query = reviews_genre.insert()
+        tables = kwargs['table']
+        if not tables:
+            tables = ['genre', 'category']
 
-        with open('static/data/genre.csv', 'r', encoding="utf-8") as file:
-            csv_reader = csv.reader(file, delimiter=",")
-            engine.execute(insert_query,
-                           ([{'id': row[0], 'name': row[1], 'slug': row[2]}
-                            for row in csv_reader]))
+        for table in tables:
+            if table == 'genre':
+                value = 'genre'
+                model = Genre
+            if table == 'category':
+                value = 'category'
+                model = Category
+
+            with open(f'static/data/{value}.csv',
+                      'r', encoding="utf-8") as file:
+                csv_reader = csv.reader(file, delimiter=",")
+                for row in csv_reader:
+                    model.objects.create(id=row[0], name=row[1], slug=row[2])
